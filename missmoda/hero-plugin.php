9c4c2e175176766280667e40446a3948d2c05afa
40D4Y5 <mohammed1alhaimi@gmail.com>
2022-01-14 19:36:09 +0800

    Update hero-plugin.php

--- missmoda/hero-plugin.php
+++ missmoda/hero-plugin.php
@@ -16,10 +16,18 @@ class WordCountAndTimePlugin
     }
     function settings()
     {
+        add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+        add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
+        register_settings('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
     }
+
+    function locationHTML(){?>
+Hello
+
+    <?php}
     function adminPage()
     {
-        add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings', array($this, 'ourHTML'));
+        add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
     }
 
     function ourHTML()
@@ -27,6 +35,13 @@ class WordCountAndTimePlugin
 
         <div class="wrap">
             <h1>Word count Settings</h1>
+            <form action="options.php" method="post">
+
+            <?php 
+            do_settings_sections('word-count-settings-page');
+            submit_button();
+            ?>
+            </form>
         </div>
 <?php }
 }
